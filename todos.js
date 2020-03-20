25662184e158c13a0dac162d00a6c26c79278a78
sasongkoadi <sasongkoadi005@gmail.com>
2020-03-20 23:50:19 +0800

    Edit function name and give comment to each function

--- todos.js
+++ todos.js
@@ -1,23 +1,24 @@
 var todos=['Item 1', 'Item 2', 'Item 3']
-
+//Show todos List
 function showTodos(){
-	console.log(todos)
+  console.log('My todos : ' ,todos)
 }
-
+// Add new todos list
 function addTodos(addNewTodo){
 	todos.push(addNewTodo)
   showTodos()
 }
-
-function removeTodos(urutan,jumlah){
+// Delete todo from list
+function deleteTodos(urutan,jumlah){
 	todos.splice(urutan-1,jumlah)
   showTodos()
 }
-
-function editTodo(angka,edit){
+// Edit Todo list
+function editTodos(angka,edit){
   todos[angka-1]=edit
   showTodos()
 }
+// Show information 
 function showInformation(){
   console.log('Add new Todos list using addTodos(tambah todo list)')
   console.log('remove Todos list using removeTodos(pilih todo,jumlah todo yang mau dihapus)')
