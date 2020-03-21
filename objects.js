f70c51e06858e5f03ac4fef1c148ca2e70ab9ba7
SasongkoAdi <sasongkoadi005@gmail.com>
2020-03-21 20:06:40 +0800

    this commit add file object

---
+++ objects.js
@@ -0,0 +1,6 @@
+var sasongkoadi ={
+        name:"Sasongko Adi",                                 Age:28,                                              sayName: function(){
+                console.log(this.name)},
+        ageNumber: function (){
+                console.log(this.Age)},
+}
