abef7bb49a9e4edfb7b0823fb0cab2481ad6cdcc
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-04-18 23:32:41 +0800

    This commit make a simple todos using node.js running on terminal

--- todosList.js
+++ todosList.js
@@ -1,2 +1,47 @@
-var todos=[]
-console.log(todos)
+var todoList = {
+  todos: [],
+
+  showTodosList: function(){
+    for(var i=0; i<this.todos.length; i++){
+      if (this.todos[i].complete === true) {
+        console.log('( X ) '+this.todos[i].kegiatan)
+      } else {
+        console.log('(   ) '+this.todos[i].kegiatan)
+      }
+    }
+  },
+
+  addTodosList: function(howMany){
+    var totalTodos=this.todos.length;
+    var count=this.todos.length;
+    for(var x=this.todos.length; x<totalTodos+howMany; x++){
+      count++;
+      this.todos.push({
+        kegiatan: 'Belajar '+count,
+        complete: false
+      });
+    }
+    this.showTodosList();
+  },
+
+  addTodo: function(newTodo){
+    this.todos.push({
+      kegiatan: newTodo,
+      complete: false,
+    })
+    this.showTodosList();
+  },
+
+  editTodo: function(number, editTodo){
+    this.todos[number-1].kegiatan = editTodo;
+    this.showTodosList();
+    
+  },
+
+  deleteTodos: function(number,howMany){
+    this.todos.splice(number-1,howMany);
+    this.showTodosList();
+  },
+
+ 
+}
