895fc6fa1f8853e9a7f6941419284816cdfafb7d
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-05-21 02:10:10 +0800

    This commit add feature edit, delete, and complete todo on list item

--- library/script.js
+++ library/script.js
@@ -1,6 +1,6 @@
 var todoList = {
   todos: [],
-  
+
   addTodosList: function(howMany){
     var totalTodos=this.todos.length;
     var count=this.todos.length;
@@ -21,36 +21,32 @@ var todoList = {
   },
 
   editTodo: function(number, editTodo){
-    this.todos[number-1].kegiatan = editTodo;
+    this.todos[number].kegiatan = editTodo;
   },
 
-  deleteTodos: function(number,howMany){
-    this.todos.splice(number-1,howMany);
+  deleteTodos: function(number){
+    this.todos.splice(number,1);
   },
 
   toggleAll: function(){
     var totalTodos= this.todos.length
     var count=0
-    for(var i=0; i<this.todos.length; i++){
-      if(this.todos[i].complete === true){
-        count++;
-      }
-    }
-    
-    if(totalTodos===count){
-      for(var x=0; x<this.todos.length; x++){
-        this.todos[x].complete = false;
-      }
-    } else {
-      for(var y=0; y<this.todos.length; y++){
-        this.todos[y].complete = true
-      }
-    }
+    this.todos.forEach(function(todo){
+      if (todo.complete === true) return count++
+    })
+    totalTodos===count?this.changeComplete(false):this.changeComplete(true)
   },
 
+  changeComplete: function(status){
+    var todo = todoList.todos
+    for(var i=0;i<todoList.todos.length;i++){
+      todo[i].complete = status; 
+    }
+  },
+  
   completeTodo: function(number){
     var todo = this.todos;
-    todo[number-1].complete = !todo[number-1].complete
+    todo[number].complete = !todo[number].complete
   }
 };
 
@@ -65,18 +61,12 @@ var handlers = {
     addTodoText.value = '';
     view.displayTodo();
   },
-  editTodo: function(){
-    var editNumberInput = document.getElementById('editNumberInput');
-    var editTextInput = document.getElementById('editTextInput');
-    todoList.editTodo(editNumberInput.valueAsNumber,editTextInput.value);
-    editNumberInput.value = '';
-    editTextInput.value = '';
+  editTodo: function(id,editTodo){
+    todoList.editTodo(id,editTodo);
     view.displayTodo();
   },
-  deleteTodo: function() {
-    var positionDeleteInput = document.getElementById('positionDeleteInput');
-    todoList.deleteTodos(positionDeleteInput.valueAsNumber,1);
-    positionDeleteInput.value = "";
+  deleteTodo: function(position) {
+    todoList.deleteTodos(position);
     view.displayTodo();
   },
   deleteAllTodos: function(){
@@ -85,10 +75,8 @@ var handlers = {
     todo.splice(0,allTodos); 
     view.displayTodo();
   },
-  toggleComplete: function() {
-    var positionToggleComplete = document.getElementById('positionToggleComplete');
-    todoList.completeTodo(positionToggleComplete.valueAsNumber);
-    positionToggleComplete.value = '';
+  toggleComplete: function(position) {
+    todoList.completeTodo(position);
     view.displayTodo();
   },
   addTodosList: function(){
@@ -108,28 +96,63 @@ var handlers = {
   backExtra: function(){
     document.getElementById('addTodoButton').innerHTML = 'Add Todo';
     document.getElementById('addTodoButton').onclick = function(){ handlers.addTodo() }
-    document.getElementById('addTodoTextInput').type = 'text';
-    document.getElementById('addTodoTextInput').placeholder = 'Write your todo';
+    document.getElementById('addTodoTextInput').type = 'text'; document.getElementById('addTodoTextInput').placeholder = 'Write your todo';
     document.getElementById('extra').innerHTML = 'Extra';
     document.getElementById('extra').onclick = function(){ handlers.hiddenExtra() }
+  },
+  editButton: function(id) {
+    view.displayTodo();
+    document.getElementById(id).innerHTML = '<input id="editInput"> <button id="editInputButton">Submit</button> <button onclick=view.displayTodo()>Cancel</button>'
+    var editTodo = document.getElementById('editInput');
+    document.getElementById('editInputButton').onclick = function() { handlers.editTodo(id, editTodo.value) }
   },
 }
 
 var view = {
+  elementReUse: {
+    ul : document.querySelector('ul'),
+  },
   displayTodo : function(){
-    var todosUl = document.querySelector('ul');
+    var todosUl = this.elementReUse.ul;
     todosUl.innerHTML='';
-    for (var i=0; i<todoList.todos.length; i++){
+    todoList.todos.forEach(function(todo,position){
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i]
-      if(todo.complete === true){
-        todoTextWithCompletion = '( X ) '+todo.kegiatan
-      } else {
-        todoTextWithCompletion = '(   ) '+todo.kegiatan
-      }
+      todo.complete === true? todoTextWithCompletion = '( X ) '+todo.kegiatan : todoTextWithCompletion = '(  ) '+todo.kegiatan
       todoLi.textContent = todoTextWithCompletion;
+      todoLi.id = position ;
+      todoLi.appendChild(this.createEditButton());
+      todoLi.appendChild(this.createDeleteButton());
+      todoLi.appendChild(this.createToggleButton());
       todosUl.appendChild(todoLi);
-    }
-  }
+    },this)
+  }, 
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
+  },
+  createEditButton: function(){
+    var editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.className = 'editButton';
+    return editButton;
+  },
+  createToggleButton: function() {
+    var toggleButton = document.createElement('button');
+    toggleButton.textContent = 'Complete'
+    toggleButton.className = 'toggleButton'
+    return toggleButton;
+  },
+  setUpEventListeners: function() {
+    var todosUl = this.elementReUse.ul
+    todosUl.addEventListener('click', function(){
+      var elementClick = event.target;
+      if (elementClick.className === 'toggleButton')  handlers.toggleComplete(parseInt(elementClick.parentNode.id)); 
+      if (elementClick.className === 'editButton') return handlers.editButton(parseInt(elementClick.parentNode.id));
+      if (elementClick.className === 'deleteButton') return handlers.deleteTodo(parseInt(elementClick.parentNode.id));
+    })   
+  },
 }
 
+view.setUpEventListeners()
