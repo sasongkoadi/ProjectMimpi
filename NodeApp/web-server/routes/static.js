b113934941b77775e962c1783c23a5943d1a69ca
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-10-27 23:27:09 +0800

    Delete web-server skeleton

--- NodeApp/web-server/routes/static.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var path = require('path')
-var static = express.static 
-
-/* Get Static Data Image */
-static(path.join(__dirname,'../public'))
-
-module.exports = static 
