d2afb01a3abb802ee848dc1d4d3179798a0d833d
Sasongko Adi <sasongko.adinugroho@qmail.id>
2020-11-15 08:33:44 +0800

    make a promise chaining playground

--- NodeApp/playground/8-promise.js
+++ NodeApp/playground/8-promise.js
@@ -10,3 +10,22 @@ testing.then((resolve) => {
 }).catch((reject) => {
     console.log(reject);
 })
+
+const sum = (a,b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        },2000)
+    })
+} 
+
+sum(3,4)
+    .then((result) => {
+        console.log(result);
+        sum(result, 300)
+    .then((result) => {
+        console.log(result)})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
